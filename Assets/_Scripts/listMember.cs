2d803031892181f6e050cfc6223ece9543dde439
Joanne <jouan.vela@gmail.com>
2016-11-18 23:54:33 +0800

    同步

--- Assets/_Scripts/listMember.cs
+++ Assets/_Scripts/listMember.cs
@@ -7,34 +7,36 @@ public class listMember : MonoBehaviour {
     public GameObject copyGameObject;//要被複製的物件
     public GameObject superGameObject;//要被放置在哪個物件底下
     private GameObject[] childGameObject;//被複製出來的物件
+	string[] members = init.membersList;
 
-    IEnumerator Start() {
-
-		string[] members = init.membersList;
+    void Start() {
 		childGameObject = new GameObject[members.Length - 1];
-
         for (int i = 0; i < members.Length - 1; i++) {
-            childGameObject[i] = Instantiate(copyGameObject);//複製copyGameObject物件
-            childGameObject[i].transform.SetParent(superGameObject.transform);//放到superGameObject物件內
-            childGameObject[i].transform.localPosition = new Vector3(0, 200 - 250 * i, 0);//複製出來的物件放置的座標為superGameObject物件內的原點
-            childGameObject[i].transform.localScale = Vector3.one;
-            childGameObject[i].name = members[i]; //將複製出來的子物件重新命名
-
-			string path = "file://" + Application.temporaryCachePath + "/member/" + members[i] + ".png";
-			WWW www = new WWW(path);
-			yield return www;
-
-			Sprite s = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), Vector2.zero);
-            childGameObject[i].GetComponent<Image>().sprite = s;
-
-            childGameObject[i].GetComponent<Image>().color = new Color(1, 1, 1, 255);
-            Button btn = childGameObject[i].GetComponent<Button>();
-            Click(btn, members[i]);
+			StartCoroutine (image(i));
         }
-        Destroy(copyGameObject);
     }
 
-    void Click(Button btn, string str) {
+	IEnumerator image(int i){
+		childGameObject[i] = Instantiate(copyGameObject);//複製copyGameObject物件
+		childGameObject[i].transform.SetParent(superGameObject.transform);//放到superGameObject物件內
+		childGameObject[i].transform.localPosition = new Vector3(0, 200 - 250 * i, 0);//複製出來的物件放置的座標為superGameObject物件內的原點
+		childGameObject[i].transform.localScale = Vector3.one;
+		childGameObject[i].name = members[i]; //將複製出來的子物件重新命名
+
+		string path = "file://" + Application.temporaryCachePath + "/member/" + members[i] + ".png";
+		WWW www = new WWW(path);
+		yield return www;
+		Sprite s = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), Vector2.zero);
+		childGameObject[i].GetComponent<Image>().sprite = s;
+
+		childGameObject[i].GetComponent<Image>().color = new Color(1, 1, 1, 255);
+		childGameObject[i].SetActive (true);
+		Button btn = childGameObject[i].GetComponent<Button>();
+		Click(btn, members[i]);
+	}
+
+    void Click(Button btn, string str){
         btn.onClick.AddListener(() => { init.mid = str; });
     }
 }
+	
