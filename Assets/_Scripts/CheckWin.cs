953596448bb63865f658585162daeed7ab2e391b
RafealYu <mp30405@gmail.com>
2016-11-19 16:16:18 +0800

    add puzzle

---
+++ Assets/_Scripts/CheckWin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckWin : MonoBehaviour {
+
+    public GameObject youWin;
+    public check check;
+    void Start() { }
+    void Update()
+    {
+        CheckGameOver();
+    }
+
+    void CheckGameOver()
+    {
+        if (check.count == 9)
+        {
+            youWin.SetActive(true);
+        }
+       
+    }
+}
---
+++ Assets/_Scripts/check.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class check : MonoBehaviour {
+
+    
+    public GameObject youWin;
+    public int count = 0;
+
+    void CheckGameOver()
+    {
+        if (count==9)
+        {
+          youWin.SetActive(true);
+        }
+    }
+}
